4d9c701a7fb7e2c35210509a2549bc333fd8113a
Marc M <gazmarcou@outlook.fr>
2022-09-11 22:48:57 +0200

    Chat front & css assets

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+
+    <div class="grid">
+        <div class="cell -3of12">
+            <div style="position: sticky;" class="menu">
+                @for ($i=0;$i<27;$i++)
+                    <a class="menu-item">MM503147<br> (172.17.2.{{ $i+1 }}) 123456789123456789</a>
+                @endfor
+            </div>
+        </div>
+        <div class="cell -1of12"></div>
+        <div class="cell -8of12">
+            <div class="alert alert-warning">Message from root:<br>Suck dicks</div>
+
+            @for ($i=0;$i<12;$i++)
+                <!-- left align -->
+                <div class="media">
+                    <div class="media-left">
+                        <div class="avatarholder">MM</div>
+                    </div>
+                    <div class="media-body">
+                        <div class="media-heading">MM503147 (172.17.2.69)</div>
+                        <div class="media-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga vel,
+                            voluptates, doloremque nesciunt illum est corrupti nostrum expedita adipisci dicta vitae?
+                            Eveniet maxime quibusdam modi molestias alias et incidunt est.
+                        </div>
+                    </div>
+                </div>
+            @endfor
+
+        </div>
+    </div>
+
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    {{--    <meta name="description" content="Webpage description goes here" />--}}
+    <meta charset="utf-8">
+    <title>NoTitle - QuickChat</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/hack.css">
+    <link rel="stylesheet" href="assets/css/hack-gray.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+    {{--    <meta name="author" content="">--}}
+</head>
+
+<body class="hack dark-grey">
+    <div class="main container">
+        {{ $slot }}
+    </div>
+</body>
+{{--<script src="https://cdn.tailwindcss.com"></script>--}}
+</html>
